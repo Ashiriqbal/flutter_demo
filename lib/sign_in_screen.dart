1018a0b300cf1a37e3f6b3969c1697a20058937a
unknown <you@example.com>
2023-01-30 17:41:32 +0500

    sign in screen

---
+++ lib/sign_in_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignScreen extends StatefulWidget {
+  const SignScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignScreen> createState() => _SignScreenState();
+}
+
+class _SignScreenState extends State<SignScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
